0c26a4b2a1f53f101cb5b5f59e9946289866659c
jyun27 <kimjyun27@gmail.com>
2025-03-19 15:57:53 +0900

    TODO 작성

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -3,6 +3,22 @@ public class Ladder {
     private final int[][] rows;
 
     public Ladder(int row, int numberOfPerson) {
+
         rows = new int[row][numberOfPerson];
+
     }
+
+    //TODO: 사다리 라인 생성
+    public void drawLine(int x, int y){
+
+    }
+
+    //TODO: 게임 실행
+    public int run(int ladderNum){
+        int result = 0;
+
+        return result;
+    }
+
+
 }
